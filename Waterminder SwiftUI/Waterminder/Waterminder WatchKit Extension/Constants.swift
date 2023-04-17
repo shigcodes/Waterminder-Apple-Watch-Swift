3e0f7cac45628ca36972b4fd2f5643c371e0ea5b
Shigrid Ann <93698919+shigcodes@users.noreply.github.com>
2023-04-17 22:30:40 +0800

    Add files via upload

---
+++ Waterminder SwiftUI/Waterminder/Waterminder WatchKit Extension/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Waterminder WatchKit Extension
+//
+//  Created by Tobias Ruano on 09/02/2021.
+//  Copyright © 2021 Shigrid Ann Docil. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultsConstant {
+    static let firstUserInteraction = "firstUserInteraction"
+    static let waterTarget = "waterTarget"
+}
